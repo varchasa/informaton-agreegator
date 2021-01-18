c070fb7deab205a2ad8e39a9513c0851afba93cd
Varchasa Aggarwal <43700017+varchasa@users.noreply.github.com>
2021-01-18 17:11:36 +0530

    raw python

--- content.py
+++ content.py
@@ -18,7 +18,7 @@ class News:
             "http://feeds.feedburner.com/ndtvnews-india-news"
         )
         print("Today's News: ")
-        for i in range(0, 20):
+        for i in range(0, 10):
             entry = newsfeed.entries[i]
             print(entry.title)
             print(entry.summary)
@@ -27,6 +27,16 @@ class News:
             print("###########################################\n")
         print('-------------------------------------------------------------------------------------------------------')
 
+    def times_ofindia(self):
+        newsfeed=feedparser.parse("https://timesofindia.indiatimes.com/rssfeeds/-2128936835.cms")
+        print("todays times of india news : ")
+        for i in range(0,10):
+            entry=newsfeed.entries[i]
+            print(entry.title)
+            print("------- News Link --------")
+            print(entry.link)
+            print("##############\n")
+        print("-------------------------------------------------------------------------------------------------")
 class Medium:
     def medium_programming(self):
         feed = feedparser.parse('https://medium.com/feed/tag/programming')
@@ -66,28 +76,27 @@ class Medium:
         print('-------------------------------------------------------------------------------------------------------')
 
 
-class StockExchange:
-    def nse_stock(self):
-        nse = Nse()
-        print("TOP GAINERS OF YESTERDAY")
-        pprint.pprint(nse.get_top_gainers())
-        print("###########################################")
-        print("TOP LOSERS OF YESTERDAY")
-        pprint.pprint(nse.get_top_losers())
-        print("###########################################")
-        print('-------------------------------------------------------------------------------------------------------')
 
 
 # objects inititalization
 # reddit_object = Reddit()
 News_object = News()
 Medium_object = Medium()
-StockExchange_object = StockExchange()
+
 
 if __name__ == "__main__":
-    News_object.Indian_News()
-    Medium_object.medium_python()
-    Medium_object.medium_programming()
-    Medium_object.medium_developer()
-    StockExchange_object.nse_stock()
+    print("1. NDTV News\n2. Times of India news\n3. Python Articles\n4. Programming Articles\n5. Developer Articles\n")
+    n=int(input("enter ur choice\n"))
+    if(n==1):
+        News_object.Indian_News()
+    elif(n==2):
+        News_object.times_ofindia()
+    elif(n==3):
+        Medium_object.medium_python()
+    elif(n==4):
+        Medium_object.medium_programming()
+    elif(n==5):
+        Medium_object.medium_developer()
+    else:
+        print("please enter valid choice")
     
